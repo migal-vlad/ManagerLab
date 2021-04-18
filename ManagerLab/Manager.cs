d59ccad757142ca0015e77a1f3ecb3b7ea80c192
migal-vlad <migal89155730056@mail.ru>
2021-04-18 17:15:05 +0300

    Lab1

---
+++ ManagerLab/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ManagerLab
+{
+    public class Manager
+    {
+        private string surname;
+        private int age;
+        private bool isGoes;
+
+        private int Age { set { age = value >= 0 ? value : 0; } }
+        private string Surname { set { surname = value.Trim(); } }
+
+        public string GetSurname() => surname;
+        public int GetAge() => age;
+        public bool GetIsGoes() => isGoes;
+
+        public void SetSurname(string surname) => Surname = surname;
+        public void SetAge(int age) => Age = age;
+        public void SetIsGoes(bool isGoes) => this.isGoes = isGoes;
+
+        public void PrintAboutObject()
+            => Console.WriteLine($"Surname: {surname}, Age: {age}, isGoes: {isGoes}");
+    }
+}
---
+++ ManagerLab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ManagerLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstManager = new Manager();
+            var secondManager = new Manager();
+
+            firstManager.SetSurname("Migal");
+            firstManager.SetAge(19);
+            firstManager.SetIsGoes(true);
+
+            secondManager.SetSurname("Feduk");
+            secondManager.SetAge(20);
+            secondManager.SetIsGoes(false);
+
+            firstManager.PrintAboutObject();
+            secondManager.PrintAboutObject();
+        }
+    }
+}
